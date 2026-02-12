6ee9152546ddc99d79d8936e39a32fcd19909dc2
Mukhammadsaiid Norbaev <norbaevmukhammadsaiid@gmail.com>
2026-02-12 21:52:41 +0000

    a little more

---
+++ 13-switches/website/index.js
@@ -0,0 +1,32 @@
+// switch
+
+// let day = 1;
+
+// switch(day){
+//     case 1:
+//         console.log("it is Monday");
+//         break;
+//     case 2:
+//         console.log("it is Tuesday");
+//         break;
+//     case 3:
+//         console.log("it is wed");
+//         break;
+//     case 4:
+//         console.log("it is thurs");
+//         break;
+//     case 5:
+//         console.log("it is froid");
+//         break;
+//     case 6:
+//         console.log("it is satur");
+//         break;
+//     case 7:
+//         console.log("it is sund");
+//         break;
+//     default:
+//         console.log(`${day} is not a day`);
+// }
+
+let testScore = 92;
+let letterGrade;
